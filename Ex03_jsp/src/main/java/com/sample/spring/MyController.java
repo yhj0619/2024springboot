3fd118da655bb4dc59f1458cf9f357b21cfd88ca
hyeji <yhj061987@gmail.com>
2024-10-02 17:03:41 +0900

    Jdbc Project

--- Ex03_jsp/src/main/java/com/sample/spring/MyController.java
+++ Ex03_jsp/src/main/java/com/sample/spring/MyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -82,5 +83,16 @@ public class MyController {
 		model.addAttribute("id", member.getId());
 		model.addAttribute("name", member.getName());
 		return "view2";
+	}
+	
+	@RequestMapping("/view3/{studentId}/{name}")
+	public String view3(
+			@PathVariable("studentId") String studentId,
+			@PathVariable("name") String name,
+			Model model
+			){
+		model.addAttribute("id", studentId);
+		model.addAttribute("name", name);
+		return "view3";
 	}
 }
