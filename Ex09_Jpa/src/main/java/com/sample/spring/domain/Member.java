6bf09516c9d27e785c6280b78ca4eec267c03f2b
hyeji <yhj061987@gmail.com>
2024-10-07 17:33:38 +0900

    Jpa Project

---
+++ Ex09_Jpa/src/main/java/com/sample/spring/domain/Member.java
@@ -0,0 +1,32 @@
+package com.sample.spring.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter 
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="test")
+public class Member {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length=100)
+	private String name;
+	
+	@Column(name="newEmail")
+	private String email;
+	private LocalDate createDate;
+}
