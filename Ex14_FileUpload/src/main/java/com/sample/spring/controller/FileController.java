5ffa4a712c48c9410cf8480a79396dbcb0008671
hyeji <yhj061987@gmail.com>
2024-10-15 17:43:22 +0900

    FileUpload Project

---
+++ Ex14_FileUpload/src/main/java/com/sample/spring/controller/FileController.java
@@ -0,0 +1,63 @@
+package com.sample.spring.controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sample.spring.service.FileDataService;
+
+@RestController
+@RequestMapping("/api")
+public class FileController {
+	
+	@Autowired
+	FileDataService fileDataService;
+	
+	
+	@PostMapping("/file")
+	public ResponseEntity<?> uploadImage(
+			@RequestParam("images") List<MultipartFile> files
+			) throws IOException{
+		List<String> uploadResult = files.stream().map(
+				file->{
+					try {
+						return fileDataService.uploadImageToFileSystem(file);
+						
+					}catch(IOException e){
+						e.printStackTrace();
+						return "failed to upload";
+					}
+					
+				}
+				).collect(Collectors.toList());
+		return ResponseEntity.status(HttpStatus.OK).body(uploadResult);
+	}
+	
+	@GetMapping("/file/{id}")
+	public ResponseEntity<?> downImage(
+			@PathVariable("id") Long id
+			) throws IOException{
+		byte[] downloadImage = fileDataService.downLoadImageFileSystem(id);
+		
+		if(downloadImage != null) {
+			return ResponseEntity.status(HttpStatus.OK)
+					.contentType(MediaType.valueOf("image/png"))
+					.body(downloadImage);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+}
---
+++ Ex14_FileUpload/src/main/java/com/sample/spring/service/FileDataService.java
@@ -0,0 +1,57 @@
+package com.sample.spring.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sample.spring.domain.FileEntity;
+import com.sample.spring.repository.FileDataRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class FileDataService {
+	
+	private final String FOLDER_PATH = "c:\\images\\";
+	private final FileDataRepository fileDataRepository;
+
+	public String uploadImageToFileSystem(MultipartFile file) throws  IOException {
+		
+		log.info("upload file : " + file.getOriginalFilename());
+		
+		String filePath = FOLDER_PATH + file.getOriginalFilename();
+		
+		FileEntity fileData = fileDataRepository.save(
+					FileEntity.builder()
+					.name(file.getOriginalFilename())
+					.type(file.getContentType())
+					.filePath(filePath)
+					.build()
+				); 
+		
+		file.transferTo(new File(filePath));
+
+		if(filePath != null) {
+			return "file uploaded success!!!! " + filePath;
+		}
+
+		return null;
+	}
+
+	public byte[] downLoadImageFileSystem(Long id) throws IOException{
+		FileEntity fileData = fileDataRepository.findById(id).orElseThrow();
+		
+		String filePath = fileData.getFilePath();
+		
+		log.info("download fileData : " + filePath);
+		
+		return Files.readAllBytes(new File(filePath).toPath());
+	}
+	
+}
