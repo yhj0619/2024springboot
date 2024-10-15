802678f081f4f736e57e2da0f1372c3b2bf40fec
hyeji <yhj061987@gmail.com>
2024-10-15 10:56:34 +0900

    Page 기능 추가

---
+++ Ex13_Api/src/main/java/com/sample/spring/repository/search/TodoSearchImpl.java
@@ -0,0 +1,41 @@
+package com.sample.spring.repository.search;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import com.querydsl.jpa.JPQLQuery;
+import com.sample.spring.model.QTodoEntity;
+import com.sample.spring.model.TodoEntity;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class TodoSearchImpl extends QuerydslRepositorySupport implements TodoSearch{
+	public TodoSearchImpl() {
+		super(TodoEntity.class);
+	}
+
+	@Override
+	public Page<TodoEntity> search1() {
+		log.info("search1###############");
+
+		QTodoEntity todoEntity = QTodoEntity.todoEntity;
+		JPQLQuery<TodoEntity> query = from(todoEntity);
+		
+		Pageable pageable = PageRequest.of(0, 10, Sort.by("tno").descending());
+		
+		query.where(todoEntity.title.contains("1"));
+		query.offset(pageable.getOffset());
+		query.limit(pageable.getPageSize());
+		query.orderBy(todoEntity.tno.desc());
+		query.fetch();
+		query.fetchCount();
+		
+		return null;
+	}
+	
+	
+}
--- Ex13_Api/src/test/java/com/sample/spring/repository/TodoRepositoryTest.java
+++ Ex13_Api/src/test/java/com/sample/spring/repository/TodoRepositoryTest.java
@@ -1,11 +1,16 @@
 package com.sample.spring.repository;
 
+
 import java.time.LocalDate;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.sample.spring.model.TodoEntity;
 
@@ -20,14 +25,17 @@ public class TodoRepositoryTest {
 	
 //	@Test
 //	public void testInsert() {
-//		TodoEntity todo = TodoEntity.builder()
-//				.title("title")
+//		for(int i=0; i<100;i++) {
+//			TodoEntity todo = TodoEntity.builder()
+//				.title("title" + i)
 //				.writer("writer...")
 //				.dueDate(LocalDate.of(2024, 10, 14))
 //				.build();
 //		
 //		TodoEntity result = todoRepository.save(todo);
 //		log.info(result);
+//		}
+//		
 //	}
 	
 //	@Test
@@ -39,15 +47,27 @@ public class TodoRepositoryTest {
 //	
 //	}
 	
+//	@Test
+//	public void testUpdate() {
+//		Long tno = 1L;
+//		Optional<TodoEntity> result = todoRepository.findById(tno);
+//		TodoEntity todo = result.orElseThrow();
+//		
+//		todo.changeTitle("update title");
+//		todo.changeComplete(true);
+//		
+//		todoRepository.save(todo);
+//	}
+	
+//	@Test
+//	public void testPaging() {
+//		Pageable pageable = PageRequest.of(0, 10, Sort.by("tno").descending());
+//		Page<TodoEntity> result = todoRepository.findAll(pageable);
+//		log.info(result.getTotalElements());
+//		log.info(result.getContent());
+//	}
 	@Test
-	public void testUpdate() {
-		Long tno = 1L;
-		Optional<TodoEntity> result = todoRepository.findById(tno);
-		TodoEntity todo = result.orElseThrow();
-		
-		todo.changeTitle("update title");
-		todo.changeComplete(true);
-		
-		todoRepository.save(todo);
+	public void testSearch1() {
+		todoRepository.search1();
 	}
 }
